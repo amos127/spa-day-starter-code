5ce3cdc49e42e82cea77ae02f05234a7b25de6e6
amy sanders <amysanders89@gmail.com>
2021-01-20 20:26:02 -0600

    Class 12 studio

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,37 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
 public class User {
+
+    @NotBlank(message = "Username required.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Valid email required.")
     private String email;
+
+    @NotBlank(message = "Password required.")
+    @Size(min = 6, message = "Password must be at least 6 characters.")
     private String password;
+
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
 
     public String getUsername() {
@@ -38,5 +56,24 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword(this.password, this.verifyPassword);
     }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword(this.password, this.verifyPassword);
+    }
+
+    private void checkPassword(String password, String verifyPassword) {
+        if(password != null
+                && verifyPassword != null
+                && !password.equals(verifyPassword)) {
+            this.verifyPassword = null;
+        }
+    }
+
 }
